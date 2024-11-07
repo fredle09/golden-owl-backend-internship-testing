43fb6f4f30431d7118a465f92d816b9c1e3697a9
fredle09 <le197903@gmail.com>
2024-11-08 05:02:18 +0700

    feat: enable global versioning for the application using URI versioning

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable versioning globally
+  app.enableVersioning({
+    type: VersioningType.URI, // We are using URI versioning like /api/v1
+    defaultVersion: '1',       // Default to version 1
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
